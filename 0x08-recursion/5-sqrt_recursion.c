ba79b0216f031315633a3648817a296875082cef
TosinK16 <tosinkasim007@gmail.com>
2022-11-03 04:44:03 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,41 +1,32 @@
 #include "main.h"
 
 /**
- * _increasevar - increases i to find sqrt
- * @i: starts at 1
- * @n: the number
- * Return: i, i + 1, or -1
+ * isSqrt - helper function for _sqrt_recursion
+ * @n: input number
+ * @i: iterator
+ * Return: square root of number
  */
 
-int _increasevar(int i, int n)
+int isSqrt(int n, int i)
 {
-	if (i * i == n)
-		return (i);
-	if (i * i < n)
-		return (_increasevar(i + 1, n));
 	if (i * i > n)
 		return (-1);
+	if (i * i == n)
+		return (i);
+	return (isSqrt(n, i + 1));
 }
+
 /**
- * _sqrt_recursion - finds sqrt
- * @n: the number
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: input number
  *
- * Return: -1, 0, or sqrt
+ * Return: square root of number, -1 if false
  */
 
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0)
-		return (0);
-	if (n == 1)
-		return (1);
-	else if (n > 1)
-	{
-		return (_increasevar(1, n));
-	}
-
-	return (-1);
+	return (isSqrt(n, 1));
 }
 
